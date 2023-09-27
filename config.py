e1412b0d585edca3faec3738dab195bd11fbbe5d
the_weaver <00van12@gmail.com>
2023-09-27 18:49:30 +0100

    Added postresql db string

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     EMAIL = os.environ.get('EMAIL')
     PASSWORD = os.environ.get('PASSWORD')
-    # uri = os.environ["DATABASE_URL"]
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-    # SQLALCHEMY_DATABASE_URI = uri
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    uri = os.environ["DATABASE_URL"]
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class ProductionConfig(Config):
     DEBUG=False
